856a5a4f3c946c6b7b93499aa021e04100b60f97
lauralyeinc <52268790+lauralyeinc@users.noreply.github.com>
2019-10-13 23:35:46 -0400

    got the axios call to work and console.log. working thru errors. Got my search bar showing, but no characters.

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -5,41 +5,37 @@ import SearchForm from './SearchForm.js';
 import CharacterCard from './CharacterCard.js'; 
 
 
-export default function CharacterList() {
-  // TODO: Add useState to track data from useEffect
-  const [characters, setCharacters] = useState({}); 
+export default function CharacterList(props) {
+  const [characters, setCharacters] = useState([]); 
   const [characterSearch, setCharacterSearch] = useState(characters);
 
   useEffect(() => {
-    // TODO: Add API Request here - must run in `useEffect`
-    //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
-    axios
-    .get('https://rickandmortyapi.com/api/character/')
-    .then(res => {
-      console.log(res); 
-      setCharacters = (characters.data.results); 
-    })
-    .catch(err => 
-      console.log(err)); 
-  }, []);
+    const getCharacters = () => {
+      axios
+        .get('https://rickandmortyapi.com/api/character/')
+        .then(res => {
+          console.log(res); 
+          setCharacters = (characters.data.results); 
+        })
+        .catch(error => {
+          console.error("Server Error", error); 
+        });
+    }
+    getCharacters();  
+    }, []);
 
   const handleChange = (event) => {
     event.preventDefault();
     let characterSearch = event.target.value; 
     setCharacterSearch(characterSearch); 
   }
-
-
   return (
     <div className='characters-page'>
-      <SearchForm handleChange={handleChange} />
-      <div className="characters-list">
-        {
-          characters.map(character => (
-            <CharacterCard key={character.id} character={character} />
-          ))
-        }
-        </div> 
+      <SearchForm handleChange={handleChange} /> 
+      {characters.map(character => (
+        <CharacterCard key={character.id} character={character}/>
+      ))}
+      
       </div>
   );
-}; 
+} 
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,13 +1,15 @@
 import React from "react";
-import { Form, Field } from "formik"; 
+
 
 
 export default function SearchForm() {
   return (
     <section className="search-form">
-     <Form>
-       <Field name="searchform" type="searchform" placeholder="Search for a Character" />
-     </Form>
+    <form>
+      <label>
+      <input name="searchform" type="text" placeholder="Search for a Character" />
+      </label>
+    </form>
     </section>
   );
 }
